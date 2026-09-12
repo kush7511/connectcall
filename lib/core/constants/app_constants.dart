d9424d88214e9e8b5c048bcfacf111290a225b8f
Kush kumar <kushk3882@gmail.com>
2026-09-12 12:43:56 +0530

    -- fixed errors -- Added new error logs for debugging -- Improved error handling

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,7 +4,7 @@ class FirestoreCollections {
   static const users = 'users';
   static const calls = 'calls';
   static const callHistory = 'call_history';
-  static const directContacts = 'direct_contacts';
+  static const directContacts = 'directContacts';
 }
 
 /// Enum-like string constants for call type / status, mirrored
